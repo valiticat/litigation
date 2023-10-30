1df26ae027b6ac9d16c52cb17ece95da7f6fcbc4
valiticat <106557000+valiticat@users.noreply.github.com>
2023-10-30 09:53:35 +0000

    11

--- Hello.py
+++ Hello.py
@@ -32,7 +32,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["test"]):
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the password.
         else:
