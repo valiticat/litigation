1c7ded96c1db2a4abc7b9892bd9c0fa33de2c2ea
valiticat <106557000+valiticat@users.noreply.github.com>
2023-10-30 07:47:27 +0000

    rename pages

--- Hello.py
+++ Hello.py
@@ -1,48 +1,26 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-
 def run():
     st.set_page_config(
         page_title="Hello",
-        page_icon="👋",
+        page_icon="⚖️",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Hello! 👋")
 
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        **👈 Для початку роботи введи код доступу**
+        ### Потім обери розділ, який тебе цікавить:
+        - Аналітика [Analytics](https://litigation.streamlit.app/Analytics)
+        - Графік засідань [Grafic](https://litigation.streamlit.app/Grafic)
+        - Судові справи [LTG](https://litigation.streamlit.app/LTG)
+        - Виконавчі провадження [VP](https://litigation.streamlit.app/VP)
     """
     )
 
