9a356b0032972c210b24fa01df62ef5de41e9711
valiticat <106557000+valiticat@users.noreply.github.com>
2023-10-30 08:37:37 +0000

    add secrets

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import hmac # Used in the authentication
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
@@ -21,10 +22,40 @@ def run():
         - Графік засідань [Grafic](https://litigation.streamlit.app/Grafic)
         - Судові справи [LTG](https://litigation.streamlit.app/LTG)
         - Виконавчі провадження [VP](https://litigation.streamlit.app/VP)
-        - test [test](https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/)
     """
     )
 
+# Authentication
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["test"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+# Main Streamlit app starts here
+st.write("Here goes your normal Streamlit app...")
+st.button("Click me")
 
 if __name__ == "__main__":
     run()
