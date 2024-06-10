4270c0baa47661441b657c2ec57b085669874b88
valiticat <106557000+valiticat@users.noreply.github.com>
2024-06-10 10:59:31 +0000

    ff

--- Hello.py
+++ Hello.py
@@ -46,7 +46,7 @@ if not check_password():
 # Connect to Database
 @st.cache_resource
 def init_connection():
-    client = pymongo.MongoClient(**st.secrets["mongo"]["ltg_db"])
+    client = pymongo.MongoClient(st.secrets["mongo"]["ltg_db"])
     return client.ltg_db.pm_tlg
 
 client = init_connection()
