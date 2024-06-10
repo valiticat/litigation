9f8b42ece566513ab9051e96f94dea24c4c48950
valiticat <106557000+valiticat@users.noreply.github.com>
2024-06-10 14:34:40 +0000

    sss

--- Hello.py
+++ Hello.py
@@ -97,9 +97,9 @@ case_num = st.text_input("Знайти інформацію за номером 
 edocs = get_ecourt_data(case_num)
 
 for edoc in edocs:
-    st.write(
-        f"""
-        📃[{edoc.get('doc_title')}](https://cabinet.court.gov.ua/document/{edoc.get('doc_eid')}), "\n",
+    st.markdown(
+        """
+        📃[{edoc.get('doc_title')}](https://cabinet.court.gov.ua/document/{edoc.get('doc_eid')}),
         🏛️{edoc.get('court_title')}
         💡{edoc.get('ops_text')}
         {clean_edoc(edoc.get('task_text'))}
