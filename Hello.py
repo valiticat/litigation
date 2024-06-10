ff5aacb0684ce9e17f3a0f16604ceb24486381c5
valiticat <106557000+valiticat@users.noreply.github.com>
2024-06-10 14:30:56 +0000

    rrr

--- Hello.py
+++ Hello.py
@@ -99,11 +99,9 @@ edocs = get_ecourt_data(case_num)
 for edoc in edocs:
     st.write(
         f"""
-        📃[{edoc.get('doc_title')}](https://cabinet.court.gov.ua/document/{edoc.get('doc_eid')})
+        📃[{edoc.get('doc_title')}](https://cabinet.court.gov.ua/document/{edoc.get('doc_eid')})\n
+        🏛️{edoc.get('court_title')}\n
+        💡{edoc.get('ops_text')}\n
+        {clean_edoc(edoc.get('task_text'))}\n
     """
     )
-
-
-        # edoc.get('court_title'),
-        # edoc.get('ops_text'),
-        # clean_edoc(edoc.get('task_text')),
